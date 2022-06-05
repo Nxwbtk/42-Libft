0bc6477279bb4b6e1ae0f339a02976533b86d235
Bunthakan Sirikamonthip <buntakan0703@gmail.com>
2022-06-05 15:24:27 +0700

    fix something

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bsirikam <bsirikam@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/04 01:11:40 by bsirikam          #+#    #+#             */
-/*   Updated: 2022/06/04 01:11:40 by bsirikam         ###   ########.fr       */
+/*   Created: 2022/06/04 21:24:10 by bsirikam          #+#    #+#             */
+/*   Updated: 2022/06/04 21:24:10 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, void *src, size_t n)
+size_t	ft_strlcpy(char *dest, char *src, size_t n)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 }
